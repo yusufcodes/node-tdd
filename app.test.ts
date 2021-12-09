eed301383de11eeb6a19ae63f435cef0b06028f8
Yusuf <40564761+yusufcodes@users.noreply.github.com>
2021-12-09 12:47:37 +0000

    Added tests before writing logic of the API

--- app.test.ts
+++ app.test.ts
@@ -11,6 +11,7 @@ describe("TODOs API", () => {
         expect(response.body).toEqual(
           expect.arrayContaining([
             expect.objectContaining({
+              id: expect.any(String),
               name: expect.any(String),
               completed: expect.any(Boolean),
             }),
@@ -19,11 +20,52 @@ describe("TODOs API", () => {
       });
   });
 
-  it("GET /todos/id -> a single TODO by ID", () => {});
+  it("GET /todos/id -> a single TODO by ID of 1", () => {
+    return request(app)
+      .get("/todos/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            name: expect.any(String),
+            completed: expect.any(Boolean),
+          })
+        );
+      });
+  });
 
-  it("GET /todos/id -> 404 if not found", () => {});
+  it("GET /todos/id -> 404 if not found", () => {
+    return request(app).get("/todos/9999").expect(404);
+  });
 
-  it("POST /todos -> create new todo", () => {});
+  it("POST /todos -> create new todo", () => {
+    return request(app)
+      .post("/todos")
+      .send({
+        name: "clean room",
+        completed: false,
+      })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            name: "clean room",
+            completed: false,
+          })
+        );
+      });
+  });
 
-  it("POST /todos -> create new todo", () => {});
+  it("POST /todos -> validates request body", () => {
+    return request(app)
+      .post("/todos")
+      .send({
+        name: 123,
+      })
+      .expect(422);
+  });
 });
